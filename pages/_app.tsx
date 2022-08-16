c0e6945d3817d15ec3ef040c4bc6e428fa16cd1a
akinori <aki10.working@gmail.com>
2022-08-16 19:41:35 +0900

    fix: add GA code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import '../src/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { createContext, Dispatch, SetStateAction, useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
+import * as gtag from '../lib/gtag'
 
 interface VisitContext {
   visited: boolean
@@ -19,10 +22,43 @@ function MyApp({ Component, pageProps }: AppProps) {
   //     setVisited(true)
   //   }
   // }, [])
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
-    <visitContext.Provider value={{visited: visited, setVisited: setVisited}}>
-      <Component {...pageProps} />
-    </visitContext.Provider>
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <visitContext.Provider value={{visited: visited, setVisited: setVisited}}>
+        <Component {...pageProps} />
+      </visitContext.Provider>
+    </>
   )
 }
 
